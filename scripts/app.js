fb000c82873d83300f82e7bb2ab6ede4cc4fb4ff
samnags <sam.nagourney@gmail.com>
2016-04-01 15:25:41 -0400

    Done

---
+++ scripts/app.js
@@ -0,0 +1,49 @@
+$(function() {
+  $('input:submit').click(app.movie.controller.show.init)
+})
+
+app = {
+
+}
+
+app.movie = {
+  model: {
+    new: (function(){
+      function Movie (title, year, genre, rated, plot) {   
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.rating = rated;
+        this.plot = plot;
+      }
+      return Movie;
+    }())
+  },
+
+  controller: {
+    show: {
+      init: function(event) {
+        event.preventDefault();
+        var movieTitle;
+        movieTitle = $('#movie').val();  
+        var promise = app.movie.adapter.getBy(movieTitle).then(function(whatever){
+          app.movie.controller.show.render(whatever)
+        })
+      },
+      // put render
+    }
+  },
+  adapter: {
+    getBy: function(movieTitle) {
+     return $.ajax({
+      method: "GET",
+      url: "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&r=json",
+      }).done(function(data) {
+        debugger
+        var movie;
+        movie = new app.movie.model.new(data.Title, data.Year, data.Genre, data.Rated, data.Plot)
+        
+     })
+    }
+  }
+}
